8de973232ad3c97b5a83054f51556b9db5e3d793
sergkuzn148 <sergkuzn.kira@yandex.ru>
2020-08-20 14:32:26 +0300

    123

---
+++ ImageToBase63/ImageToBase63/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ImageToBase63
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
